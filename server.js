7a8ae9c043d27dd26b25a238f4c1827e93999123
mtran247 <157142565+mtran247@users.noreply.github.com>
2024-01-20 13:19:50 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,86 +2,42 @@
 
 // server.js sets up the Express application, specifies the API routes, and starts the server to listen for incoming requests.
 
-
-
-
-
 // import dependencies
-
 const express = require('express');
-
 const bodyParser = require('body-parser');
-
-const cors = require('cors')
-
+const cors = require('cors');
 const { log, ExpressAPILogMiddleware } = require('@rama41222/node-logger');
-
-
-
 const apiRoutes = require('./routes/api');
 
-
-
 // server config
-
 let config = {
-
-name: 'melissa-everyrealm-burrito-api',
-
-port: 3000,
-
-host: '0.0.0.0',
-
+    name: 'melissa-everyrealm-burrito-api',
+    port: 3000,
+    host: '0.0.0.0',
 };
 
-
-
 // create object for express application
-
 const app = express();
-
 const orders = {};
 
-
-
 // logger config
-
 const logger = log({ console: true, file: false, label: config.name });
 
-
-
 app.use(bodyParser.json());
-
 app.use(cors());
-
 app.use(ExpressAPILogMiddleware(logger, { request: true }));
 
-
-
 // tells Express to use the routes defined in the apiRoutes module when the path starts with '/api'.
-
 app.use('/api', apiRoutes);
 
-
-
 // starts the Express server and logs out the server config
-
 if (process.env.NODE_ENV != 'test') {
-
-app.listen(config.port, config.host, (e)=> {
-
-if(e) {
-
-throw new Error('Internal Server Error');
-
+    app.listen(config.port, config.host, (e) => {
+        if (e) {
+            throw new Error('Internal Server Error');
+        }
+        logger.info(`${config.name} running on ${config.host}:${config.port}`);
+    });
 }
-
-logger.info(`${config.name} running on ${config.host}:${config.port}`);
-
-});
-
-}
-
-
 
 module.exports = { app, orders };
